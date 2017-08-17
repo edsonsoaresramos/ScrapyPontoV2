e853bbec900d522dc32d6f20f3687cf4602bf22d
Edson Soares <edson.soares.r@gmail.com>
2017-08-17 16:56:01 -0300

    leitura de e-amil

--- ScrapyPontoV2.py
+++ ScrapyPontoV2.py
@@ -1,93 +1,34 @@
-#-------------------------------------------------------------------------------
-# Name:        read_email.py
-# Purpose:     Retrieve All the emails from Gmail
-#
-# Author:      Kiran Chandrashekhar
-#
-# Created:     20/06/2016
-# Copyright:   (c) kiran 2016
-# Licence:      In the spirit of sharing knowledge and happiness,
-#               You can copy, edit, share
-#-------------------------------------------------------------------------------
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
-
-
-import sys
-from datetime import datetime, timedelta
-import os
-import re
+import smtplib
+import time
 import imaplib
 import email
 
+SMTP_SERVER = "imap.gmail.com"
+SMTP_PORT   = 993
 
-# # Read only emails from last 3 days
-no_days_query = 1
+def readmail(emailAddress, pwd):
+    try:
+        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+        mail.login(emailAddress, pwd)
+        mail.select('inbox')
+        type, data = mail.search(None, 'ALL')
+        mail_ids = data[0]
+        id_list = mail_ids.split()
 
-server = "imap.gmail.com"
-port_num = 993
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
 
+        for i in range(latest_email_id, first_email_id, -1):
+            typ, data = mail.fetch(str(i), '(RFC822)')
 
-def read_email1(gmail_user, gmail_pwd):
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(str(response_part[1]))
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    print('From : ' + str(email_from) + '\n')
+                    print('Subject : ' + str(email_subject) + '\n')
+    except(Exception):
+        print(str(Exception))
 
-    conn = imaplib.IMAP4_SSL(server, port_num)
-    conn.login(gmail_user, gmail_pwd)
-    conn.select()
-
-    #Check status for 'OK'
-    status, all_folders = conn.list()
-
-    folder_to_search = 'INBOX'
-
-    #Check status for 'OK'
-    status, select_info = conn.select(folder_to_search)
-
-    if status == 'OK':
-        today = datetime.today()
-        cutoff = today - timedelta(days=no_days_query)
-
-        from_email = ''
-        #from_email = 'contact@sapnaedu.in'
-
-        search_key = from_email + " after:" + cutoff.strftime('%Y/%m/%d')
-
-        status, message_ids = conn.search(None, 'X-GM-RAW', search_key)
-
-
-        for id in message_ids[0].split():
-            status, data = conn.fetch(id, '(RFC822)')
-
-            email_msg = email.message_from_string(data[0][1])
-
-            #Print all the Attributes of email message like Subject,
-            #print email_msg.keys()
-
-            subject = email_msg['Subject']
-            sender_email =  email_msg['From']
-            sent_to_email =  email_msg['To']
-
-            for part in email_msg.walk():
-                if part.get_content_type() == 'text/plain':
-                    email_content = part.get_payload() # prints the raw text
-                    #TODO :
-                    #process_email() #Delete, Mark as Spam, Forward it
-                    #TODO :
-                    #print email_content
-
-    else:
-        print( "Error")
-    ## Search for relevant messages
-    ## see http://tools.ietf.org/html/rfc3501#section-6.4.5
-
-if __name__ == '__main__':
-
-    # Gmail Configuration
-    gmail_user = ''
-    gmail_pwd = ''
-
-    ret = read_email1(gmail_user, gmail_pwd)
-
+readmail('r3im0z@gmail.com', '50p#144rt#ur')
